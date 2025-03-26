4f3d3ffaa4a284a1182a4ba74d017cf75977af36
Rahul <83376754+himalayanpeaks@users.noreply.github.com>
2025-03-26 20:34:52 +0100

    Added HAL layer

---
+++ OneDriver.Framework/OneDriver.Framework/Module/IHalLayer.cs
@@ -0,0 +1,17 @@
+﻿using OneDriver.Framework.Libs.Announcer;
+using OneDriver.Framework.Libs.Validator;
+
+namespace OneDriver.Framework.Module
+{
+    public interface IDeviceHAL<TInternalDataHAL> where TInternalDataHAL : BaseDataForAnnouncement, new()
+    {
+        public delegate void ProcessDataAnnouncer(TInternalDataHAL dataHAL);
+        ConnectionError Open(string initString, IValidator validator);
+        ConnectionError Close();
+        void StartProcessDataAnnouncer();
+        void StopProcessDataAnnouncer();
+        void AttachToProcessDataEvent(DataTunnel<TInternalDataHAL>.DataEventHandler processDataEventHandler);
+        int NumberOfChannels { get; }
+    }
+}
+
